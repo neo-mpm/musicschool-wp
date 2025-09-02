47f229e62fb204c4d146e36c1c1d8838d06e22dc
neo-mpm <sb_mode@hotmail.co.jp>
2025-09-02 22:44:41 +0900

    Revert "docs(search): キーワードのマッチと入力が無い場合の処理の変更、h1タグに変更"
    This reverts commit 6690ec3cf8ddfd5f4df614e20a281c677cad042e.

--- musicschool/search.php
+++ musicschool/search.php
@@ -66,10 +66,7 @@ get_header();
         else :
         ?>
           <div class="search-content__no-result">
-            <div class="search-content__head search-content-head blog-content__head">
-              <h1 class="search-content-head__text">「<span class="search-content-head__text--bold"><?= get_search_query() ?></span>」の検索結果</h1>
-              <p class="search-content-head__text">0件</p>
-            </div>
+            <p>検索されたキーワードにマッチする<br class="br-sp">記事はありませんでした。</p>
             <a onclick="history.back()" class="contact__button contact-button contact-button--send button">戻る</a>
           </div>
         <?php
@@ -77,7 +74,7 @@ get_header();
       else:
         ?>
         <div class="search-content__no-result">
-          <h1>検索キーワードが未入力です。</h1>
+          <p>検索キーワードが未入力です。</p>
           <a onclick="history.back()" class="contact__button contact-button contact-button--send button">戻る</a>
         </div>
       <?php
